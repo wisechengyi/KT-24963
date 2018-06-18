bb71f64a4f8b5f0f16c071f438d00fd2b6e530d8
wisechengyi <wisechengyi@gmail.com>
2018-06-18 13:31:48 -0700

    repro

---
+++ lib/squabble/src/main/java/example/SquabbleProcessor.kt
@@ -0,0 +1,23 @@
+package example
+
+import javax.annotation.processing.AbstractProcessor
+import javax.annotation.processing.Processor
+import javax.annotation.processing.RoundEnvironment
+import javax.annotation.processing.SupportedSourceVersion
+import javax.lang.model.SourceVersion
+import javax.lang.model.element.TypeElement
+
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+@AutoService(Processor::class)
+class SquabbleProcessor : AbstractProcessor() {
+    override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    companion object {
+        private const val GENERATED_SUFFIX = "\$\$Args"
+        private const val SERIALIZERS_SUFFIX = "\$\$Serializers"
+        private const val ACTIVITY_ARGS_INTERFACE = "com.example.app.common.activity.ActivityArgs"
+        private const val FRAGMENT_ARGS_INTERFACE = "com.example.app.common.base.FragmentArgs"
+    }
+}
